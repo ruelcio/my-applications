6a1eaef926611013b6550d1ede1000aeea39a7f5
ado-dc <jdc67903@egmail.com>
2026-09-04 20:48:41 +0100

    feat implement crud

---
+++ src/main/java/ao/my_applications/my_app/controller/CandidaturaController.java
@@ -0,0 +1,33 @@
+package ao.my_applications.my_app.controller;
+
+import ao.my_applications.my_app.model.Candidatura;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/candidaturas")
+public class CandidaturaController {
+
+    private final List<Candidatura> candidaturas = new ArrayList<>();
+
+    private Long id = 1L;
+
+	@PostMapping
+	public ResponseEntity<Candidatura> criarCandidatura(
+        @RequestBody Candidatura candidatura) {
+
+    candidatura.setId(id);
+    id++;
+
+    candidaturas.add(candidatura);
+
+    return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(candidatura);
+	}
+}
---
+++ src/main/java/ao/my_applications/my_app/model/Candidatura.java
@@ -0,0 +1,52 @@
+package com.example.candidaturas.model;
+package ao.my_applications.my_app.model;
+
+public class Candidatura {
+
+    private Long id;
+    private String nomeCandidato;
+    private String vaga;
+    private String email;
+
+    public Candidatura() {
+    }
+
+    public Candidatura(Long id, String nomeCandidato, String vaga, String email) {
+        this.id = id;
+        this.nomeCandidato = nomeCandidato;
+        this.vaga = vaga;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public String getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(String vaga) {
+        this.vaga = vaga;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
